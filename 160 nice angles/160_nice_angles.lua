42db821ada7e3fb00f0446ade6e4858b86a407ae
Matthew Davey <github@project-2501.net>
2016-03-17 16:38:42 +1100

    I honestly don't know how it works
    I mean, I wrote it, but I don't know how I got to that solution  (my
    brain is sure it's wrong)

---
+++ 160 nice angles/160_nice_angles.lua
@@ -0,0 +1,30 @@
+
+local function nice_angles(line)
+    local decimal, fractional = line:match('^(%d+)(%.%d+)$')
+    decimal, fractional = tonumber(decimal), tonumber(fractional)
+    local minutes = math.floor(fractional * 0.60 * 100)
+    fractional = fractional - ((minutes / 100) / 0.60)
+    local seconds = math.floor((fractional * 0.60) * 0.60 * 100 * 100)
+    return string.format('%0d.%02d\'%02d"', decimal, minutes, seconds)
+end
+
+
+local function codeeval(f)
+        
+    -- normal
+    if arg[0]:find('Users\\Matthew') == nil then        
+        for line in io.lines(arg[1]) do
+            if line ~= '' then
+                print(f(line))
+            end
+        end
+        
+    -- development
+    else
+        local dev_enviroment = require("../codeeval")
+        dev_enviroment.run_local(f)
+    end
+end
+
+
+codeeval(nice_angles)
